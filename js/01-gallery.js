be1e540655f270f404b76cc63fe03b46d60af0d9
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2022-12-20 16:28:13 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+gallery.addEventListener('click', onclick)
+
+const markup = galleryItems.map(({preview, original, description }) =>
+`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join("")
+
+gallery.innerHTML = markup
+
+function onclick(evt) {
+    evt.preventDefault()
+    // console.dir(evt.target)
+
+    if(evt.target.className === "gallery" ){
+    return
+    }
+
+    const instance = basicLightbox.create(`
+      <img
+      src="${evt.target.dataset.source}"
+     >
+    `)
+    if (!instance.show()) {
+       return
+    }
+
+    document.addEventListener('keydown', (evt) => {
+    if (evt.code === 'Escape') {
+        instance.close();
+  }
+})
+
+}
+
+// console.log(basicLightbox)
+
+// const instance = basicLightbox.create(`<div class="gallery__item">
+// //   <a class="gallery__link" href="${original}">
+// //     <img
+// //       class="gallery__image"
+// //       src="${preview}"
+// //       data-source="${original}"
+// //       alt="${description}"
+// //     />
+// //   </a>
+// // </div>`).join("")
+
+// instance.show()
+
