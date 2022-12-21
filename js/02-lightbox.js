cd0167cdf9904646070976e9699ebc4a8fa9a6ee
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2022-12-21 12:34:47 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const gallery = document.querySelector('.gallery')
 
 const markup = galleryItems.map(({preview, original, description }) =>
-`<div class="gallery__item">
+`<li class="gallery__item">
 <a class="gallery__item" href="${original}">
   <img 
   class="gallery__image" 
   src="${preview}" 
   alt="${description}"
  />
-</a></div>`).join("")
+</a>
+</li>`).join("")
 
 gallery.innerHTML = markup
 
